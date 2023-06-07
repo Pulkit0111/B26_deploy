d5b2b8deb605f85e014cd4368c1c2cfc14fb4e92
Pulkit0111 <ptyagi1551@gmail.com>
2023-06-07 22:49:18 +0530

    learn to deploy

---
+++ routes/note.routes.js
@@ -0,0 +1,69 @@
+const express=require("express")
+const {NoteModel}=require("../models/note.model")
+const {auth}=require("../middlewares/auth.middleware")
+
+const noteRouter=express.Router()
+
+noteRouter.use(auth)
+noteRouter.post("/create",async(req,res)=>{
+    try{
+        const note=new NoteModel(req.body)
+        await note.save()
+        res.json({msg:"New note has been added",note:req.body})
+    }catch(err){
+        res.json({error:err.message})
+    }
+})
+
+noteRouter.get("/",async (req,res)=>{
+    try{
+        const notes=await NoteModel.find({userID:req.body.userID})
+        res.send(notes)
+    }catch(err){
+        res.json({error:err.message})
+    }
+})
+
+noteRouter.patch("/update/:noteID",async (req,res)=>{
+    //userID in the user doc === userID in the note doc
+    const userIDinUserDoc=req.body.userID
+    const {noteID}=req.params
+    try{
+        const note=await NoteModel.findOne({_id:noteID})
+        const userIDinNoteDoc=note.userID
+        if(userIDinUserDoc===userIDinNoteDoc){
+            // console.log("userID in User Doc",userIDinUserDoc, "userID in Note Doc", userIDinNoteDoc)
+            await NoteModel.findByIdAndUpdate({_id:noteID},req.body)
+            res.json({msg:`${note.title} has been updated`})
+        } else {
+            // console.log("userID in User Doc",userIDinUserDoc, "userID in Note Doc", userIDinNoteDoc)
+            res.json({msg:"Not Authorized!!"})
+        }
+    } catch(err){
+        res.json({error:err})
+    }
+})
+
+noteRouter.delete("/delete/:noteID",async (req,res)=>{
+        //userID in the user doc === userID in the note doc
+        const userIDinUserDoc=req.body.userID
+        const {noteID}=req.params
+        try{
+            const note=await NoteModel.findOne({_id:noteID})
+            const userIDinNoteDoc=note.userID
+            if(userIDinUserDoc===userIDinNoteDoc){
+                // console.log("userID in User Doc",userIDinUserDoc, "userID in Note Doc", userIDinNoteDoc)
+                await NoteModel.findByIdAndDelete({_id:noteID})
+                res.json({msg:`${note.title} has been deleted`})
+            } else {
+                // console.log("userID in User Doc",userIDinUserDoc, "userID in Note Doc", userIDinNoteDoc)
+                res.json({msg:"Not Authorized!!"})
+            }
+        } catch(err){
+            res.json({error:err})
+        }
+})
+
+module.exports={
+    noteRouter
+}
